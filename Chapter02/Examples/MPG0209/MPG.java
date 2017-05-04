7db215cc8940a3747620829cafeeb2db82ea686c
ethan mcgimsey <ethan mcgimsey>
2017-05-04 19:18:53 -0400

    Get miles and gallons from keyboard.

--- Chapter02/Examples/MPG0209/MPG.java
+++ Chapter02/Examples/MPG0209/MPG.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+
 public class MPG {
 
 	public static void main(String[] args) {
 
+		Scanner keyboard = new Scanner(System.in);
+
 		// ask (prompt) the user for number of miles driven 
+		 System.out.println("Enter the number of miles driven: ");
+
 		// get number of miles from user
+		 int miles = keyboard.nextInt();
 
 		// ask (prompt) user for number of gallons used
+		  System.out.println("Enter the number of gallons used: ");
+
 		// get number of gallons from the user
+		  int gallons = keyboard.nextInt();
 
 		// calculate MPG
+
+
 		// MPG = Miles driven / Gallons of gas used.
 
 
 		// display result to user
-		
+
+
 	}
 }
