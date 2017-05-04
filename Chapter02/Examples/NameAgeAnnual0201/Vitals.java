5d2922d8def538f87a775a3e4dfcb9c9d8a2f67b
ethan mcgimsey <ethan mcgimsey>
2017-05-04 18:20:01 -0400

    Assigned values to variables.

--- Chapter02/Examples/NameAgeAnnual0201/Vitals.java
+++ Chapter02/Examples/NameAgeAnnual0201/Vitals.java
@@ -1,6 +1,17 @@
 public class Vital {
      
      public static void main(String arg[]) {
+           // declare variables
+           String name;
+           int age;
+           double annualPay;
            
+           // assign values to variables
+           name = "Ethan McGimsey";
+           age = 20;
+           annualPay = 125000.0;
+          // display variables
+
+         
      }
 }
